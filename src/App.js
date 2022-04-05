7da9426177747160846c9eec7fe162a25eb8da3a
Adarshcodes <adarsh.lrnr@gmail.com>
2022-04-05 17:54:26 +0530

    fetching advice from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./assets/sass/main.css";
 
 import Card from "./components/Card";
 
 function App() {
-	// https://api.adviceslip.com/advice
+	const [advice, setAdvice] = useState([]);
+
+	useEffect(() => {
+		adviceFetcher();
+	}, []);
+
+	function adviceFetcher() {
+		fetch(`https://api.adviceslip.com/advice`)
+			.then((res) => res.json())
+			.then((data) => {
+				setAdvice(data.slip);
+			});
+	}
+
+	console.log(advice);
 
 	return (
 		<main className="main-container">
-			<Card />
+			<Card
+				id={advice.id}
+				adviceText={advice.advice}
+				fetchAdvice={adviceFetcher}
+			/>
 		</main>
 	);
 }
