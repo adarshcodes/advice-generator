4f1ab569a99234ee95cddeb4213f66ad191ff36d
Adarshcodes <adarsh.lrnr@gmail.com>
2022-04-06 21:46:41 +0530

    adding responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import Divider from "../assets/images/pattern-divider.svg";
 export default function Card(props) {
 	return (
 		<div className="card">
-			<h1 className="card__advice-number">Sigma Rule #{props.id}</h1>
+			<h1 className="card__advice-number">Advice #{props.id}</h1>
 			<h2 className="card__advice">{props.adviceText}</h2>
 			<img src={Divider} alt="divider" className="card__divider" />
 			<Dice handleChange={props.fetchAdvice} />
